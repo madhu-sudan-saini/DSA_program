6c865789175c97311457e9f7553ea1267d0f555b
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2026-08-10 23:45:48 +0530

    DSA code
    Implement a function to print the left view of a binary tree.

---
+++ Tree_left_view.cpp
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<map>
+#include<stack>
+using namespace std;
+
+class Node
+{
+    public:
+
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int d)
+    {
+        data=d;
+        left=NULL;
+        right=NULL;
+    }
+};
+
+
+static int idx= -1;
+Node* builtTree(vector<int> &Preorder)
+{
+    idx++;
+
+    if(idx >= Preorder.size() || Preorder[idx]==-1)
+    {
+        return NULL;
+    }
+
+    Node* root=new Node(Preorder[idx]);
+    root->left=builtTree(Preorder);
+    root->right=builtTree(Preorder);
+
+    return root;
+}
+void leftview(Node* root)
+{
+    queue<pair<Node* ,int>> q;
+    map<int,int> m;
+
+    q.push({root,0});
+
+    while(!q.empty())
+    {
+        Node* curr=q.front().first;
+        int level=q.front().second;
+        q.pop();
+
+        if(m.find(level)==m.end())
+        {
+            m[level]=curr->data;
+        }
+
+        if(curr->left !=NULL)
+        {
+            q.push({curr->left,level+1});
+        }
+
+        if(curr->right !=NULL)
+        {
+            q.push({curr->right,level+1});
+        }
+    }
+
+    cout << "left View: ";
+
+    for(auto val : m)
+    {
+        cout<<val.second<<" ";
+    }
+    cout<<endl;
+    
+}
+
+int main()
+{
+    vector<int> Preorder={1,2,4,-1,-1,5,-1,-1,3,6,-1,-1,7,-1,-1};
+
+    Node* root=builtTree(Preorder);
+    leftview(root);
+}
