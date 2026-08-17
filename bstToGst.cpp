bdced063a7fffc0bbf1d3e0148cd6a1dcfde4d6c
Madhu Sudan Saini <sainimadhu2005@gmail.com>
2026-08-17 17:42:45 +0530

    DSA code

---
+++ bstToGst.cpp
@@ -0,0 +1,26 @@
+
+class Solution {
+public:
+
+    int sum=0;
+    void solve(TreeNode* root)
+    {
+        if(root == NULL)
+        {
+            return;
+        }
+
+        solve(root->right);
+
+        sum=sum+root->val;
+        root->val=sum;
+
+        solve(root->left);
+
+    }
+    TreeNode* bstToGst(TreeNode* root) {
+
+       solve(root); 
+       return root;
+    }
+};
